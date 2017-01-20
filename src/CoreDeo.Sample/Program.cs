61f0493c4b76146fa08852e4950fca8475edebc6
aloisdg <aloisdegouvello@live.fr>
2017-01-20 16:55:43 +0100

    Print class with sample

---
+++ src/CoreDeo.Sample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDeo.Sample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var c = new Class1();
+            Console.WriteLine(c.IsWorking);
+        }
+    }
+}
