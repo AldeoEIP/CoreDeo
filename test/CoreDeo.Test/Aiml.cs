5a7ddf57b9ffce9a356b0993844afd13b8ad014c
aloisdg <aloisdegouvello@live.fr>
2017-01-24 23:22:08 +0100

    Add test

---
+++ test/CoreDeo.Test/Aiml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CoreDeo.Test {
+    public class Aiml {
+        [Fact]
+        public async void CanAnswer() {
+            const string question = "Quel est mon nom ?";
+            const string login = "Kevin69";
+            const string password = "DUCHAMP";
+            var client = new Client ();
+
+            var connected = await client.LoginAsync (login, password);
+            Assert.True (connected);
+
+            var answer = await client.GetAnswerAsync (question);
+            Assert.Equal ("ton nom est Toto.", answer);
+        }
+
+        [Fact]
+        public async void CanCalcul() {
+            const string question = "Combien font 2 + 2 ?";
+            const string login = "Kevin69";
+            const string password = "DUCHAMP";
+            var client = new Client ();
+
+            var connected = await client.LoginAsync (login, password);
+            Assert.True (connected);
+
+            var answer = await client.GetAnswerAsync (question);
+            Assert.Equal ("4", answer);
+        }
+
+        [Fact]
+        public async void CanAvoidEmpty() {
+            const string question = "";
+            const string login = "Kevin69";
+            const string password = "DUCHAMP";
+            var client = new Client ();
+
+            var connected = await client.LoginAsync (login, password);
+            Assert.True (connected);
+
+            await Assert.ThrowsAsync<HttpRequestException> (async () => await client.GetAnswerAsync (question));
+        }
+    }
+}
---
+++ test/CoreDeo.Test/Connection.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http;
+using Xunit;
+
+namespace CoreDeo.Test {
+    public class Connection {
+        [Fact]
+        public async void CanConnect() {
+            //public void CanConnect() {
+            const string login = "Kevin69";
+            const string password = "DUCHAMP";
+            var client = new Client ();
+
+            var connected = await client.LoginAsync (login, password);
+            //var connected = client.LoginAsync (login, password);
+
+            Assert.True (connected);
+        }
+
+        [Fact]
+        //public async void CantConnect() {
+        public async void CantConnect() {
+            const string login = "";
+            const string password = "";
+            var client = new Client ();
+
+            //var connected = await client.LoginAsync (login, password);
+            //var connected = ;
+
+            await Assert.ThrowsAsync<HttpRequestException>(async () => await client.LoginAsync (login, password));
+        }
+    }
+}
